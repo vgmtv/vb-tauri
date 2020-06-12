8ca0d164ba0411cff5a90d8fe9970bbba9a29e13
nampdn <nampdn@protonmail.com>
2020-06-12 08:13:34 +0700

    feat: 🎸 ipfs
    able to run ipfs server

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,49 +11,13 @@ use std::{
 };
 use tauri::{Handle};
 
-fn main() {// Register a listener to the "js-event" event
+fn main() {
   tauri::AppBuilder::new()
     .setup(|_webview, _| {
         let handle_clone = _webview.handle().clone();
         std::thread::spawn(move || {
             spawn_vietbible_flutter(&handle_clone);
         });
-    })
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            PerformRequest {
-              endpoint,
-              body,
-              callback,
-              error,
-            } => {
-              // tauri::execute_promise is a helper for APIs that uses the tauri.promisified JS function
-              // so you can easily communicate between JS and Rust with promises
-              tauri::execute_promise(
-                _webview,
-                move || {
-                  println!("{} {:?}", endpoint, body);
-                  // perform an async operation here
-                  // if the returned value is Ok, the promise will be resolved with its value
-                  // if the returned value is Err, the promise will be rejected with its value
-                  // the value is a string that will be eval'd
-                  Ok("{ message: 'Hello World from Rust!' }".to_string())
-                },
-                callback,
-                error,
-              )
-            }
-          }
-          Ok(())
-        }
-      }
     })
     .build()
     .run();
@@ -69,10 +33,9 @@ fn get_bin_command(name: &str) -> String {
 fn spawn_vietbible_flutter<T: 'static>(handle: &Handle<T>) {
     // Get path to orchestrator and main binary
     let ipfs_binary = get_bin_command("ipfs");
-    let vietbible_binary = get_bin_command("Viet.Bible.app");
-    println!("IPFS Binary: {}", ipfs_binary);
     let stdout = Command::new(ipfs_binary)
-        .args(vec!["deamon", "--init"])
+        .args(vec!["daemon", "--init"])
+        .env("IPFS_PATH", "~/.ipfs")
         .stdout(Stdio::piped())
         .spawn()
         .expect("EXIT STATUS")
@@ -85,6 +48,7 @@ fn spawn_vietbible_flutter<T: 'static>(handle: &Handle<T>) {
         .lines()
         .filter_map(|line| line.ok())
         .for_each(|line| {
+            print!("{}", line);
             if line.starts_with("Daemon is ready") {
                 let url = "http://127.0.0.1:5001/webui";
                 if !vietbible_started {
@@ -92,6 +56,20 @@ fn spawn_vietbible_flutter<T: 'static>(handle: &Handle<T>) {
                     handle
                         .dispatch(move |webview| webview.eval(&format!("window.location.replace('{}')", url)))
                         .expect("failed to init app");
+                    // let vietbible_binary = get_bin_command("Viet.Bible.app");
+                    // print!("VB Bin: {}", vietbible_binary);
+                    // let stdout_flutter = Command::new(vietbible_binary)
+                    //     .stdout(Stdio::piped())
+                    //     .spawn()
+                    //     .expect("")
+                    //     .stdout.expect("Error");
+                    // let reader_vb = BufReader::new(stdout_flutter);
+                    // reader_vb
+                    //     .lines()
+                    //     .filter_map(|line| line.ok())
+                    //     .for_each(|line| {
+                    //         println!("Message from flutter: {}", line);
+                    //     });
                 }
             }
         })
